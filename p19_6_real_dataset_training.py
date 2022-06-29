ac53f9ca9db7863e617a99175cba8c92ad8babd9
dpflores <delpiero22.flores@gmail.com>
2022-06-29 12:24:24 -0500

    training done

--- p19_6_real_dataset_training.py
+++ p19_6_real_dataset_training.py
@@ -72,17 +72,17 @@ X_test = (X_test.reshape(X_test.shape[0], -1).astype(np.float32) - 127.5)/127.5
 model = Model()
 
 # Add layers
-model.add(Layer_Dense(X.shape[1], 64))
+model.add(Layer_Dense(X.shape[1], 128))
 model.add(Activation_ReLU())
-model.add(Layer_Dense(64, 64))
+model.add(Layer_Dense(128, 128))
 model.add(Activation_ReLU())
-model.add(Layer_Dense(64 , 10))
+model.add(Layer_Dense(128 , 10))
 model.add(Activation_Softmax())
 
 # Set loss, optimizer and accuracy objects
 model.set(
     loss = Loss_CategoricalCrossentropy(),
-    optimizer = Optimizer_Adam(decay = 5e-5),
+    optimizer = Optimizer_Adam(decay = 1e-3),
     accuracy = Accuracy_Categorical()
 )
 
@@ -91,4 +91,4 @@ model.finalize()
 
 
 # Train the model
-model.train(X, y, validation_data=(X_test, y_test), epochs=5 , batch_size=128 , print_every=100)
+model.train(X, y, validation_data=(X_test, y_test), epochs=10 , batch_size=128 , print_every=100)
